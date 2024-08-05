a9530d03fceba4d617925901b21a2224f4005ca0
Tanvi Yamarthy <125161619+tyamarthy@users.noreply.github.com>
2024-08-05 00:22:26 -0400

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,9 @@
+import RalewayWoff2 from './fonts/Raleway-Regular.woff2';
+import { createTheme } from '@mui/material/styles';
+const theme = createTheme({
+    typography: {
+      fontFamily: 'Raleway, sans-serif', // Set your global font here
+    },
+  });
+  
+  export default theme;
